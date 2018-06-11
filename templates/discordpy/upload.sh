f85325feaa334ab9a4d9c9e93c25e4303d129ad4
Gorialis <sansgorialis@gmail.com>
2018-06-12 01:19:51 +0900

    Fix error in docker image upload script

--- templates/discordpy/upload.sh
+++ templates/discordpy/upload.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 {% for version, distro, checkout, stage in variations -%}
-{%- for tagdata in product(version, distro, checkout, stage) -%}
+{%- for tagdata in product(version[1], distro[1], checkout[1], stage[1]) -%}
 if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:{{ mktag(*tagdata) }}; then echo -e "UPLOAD {{ mktag(*tagdata) }}" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "UPLOAD {{ mktag(*tagdata) }}" >> ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
 {% endfor %}
 {%- endfor %}
