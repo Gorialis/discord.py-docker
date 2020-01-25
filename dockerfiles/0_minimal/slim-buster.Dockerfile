6451333684e7aaf88196c73ffd83060b055b67d2
Devon R <sansgorialis@gmail.com>
2020-01-26 01:16:47 +0900

    Remove slim for now

--- dockerfiles/0_minimal/slim-buster.Dockerfile
+++ dockerfiles/0_minimal/slim-buster.Dockerfile
@@ -10,7 +10,7 @@ FROM python:$PYTHON_VERSION-slim-buster
 ARG BUILD_TIME=unknown
 ARG GIT_HEAD=unknown
 LABEL maintainer="Devon R <Gorialis>"
-LABEL creation_time="2020-01-25 15:57:57 UTC"
+LABEL creation_time="2020-01-25 16:17:27 UTC"
 LABEL build_time=$BUILD_TIME
 LABEL git_head=$GIT_HEAD
 
--- dockerfiles/1_full/upload.sh
+++ dockerfiles/1_full/upload.sh
@@ -2,5 +2,4 @@
 set -euo pipefail
 
 if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build1-$1-buster; then echo -e "UPLOAD build1-$1-buster" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build1-$1-buster" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
-if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build1-$1-slim-buster; then echo -e "UPLOAD build1-$1-slim-buster" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build1-$1-slim-buster" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
 if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build1-$1-alpine; then echo -e "UPLOAD build1-$1-alpine" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build1-$1-alpine" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
