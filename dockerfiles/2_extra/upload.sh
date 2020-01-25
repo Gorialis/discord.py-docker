3cf063e1c6d4a2c32f586293775a7e7fb11292ab
Devon R <sansgorialis@gmail.com>
2020-01-26 01:33:01 +0900

    Forget about 3.9 for now

--- dockerfiles/2_extra/upload.sh
+++ dockerfiles/2_extra/upload.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 
 if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build2-$1-buster; then echo -e "UPLOAD build2-$1-buster" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build2-$1-buster" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
+if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build2-$1-slim-buster; then echo -e "UPLOAD build2-$1-slim-buster" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build2-$1-slim-buster" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
 if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build2-$1-alpine; then echo -e "UPLOAD build2-$1-alpine" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build2-$1-alpine" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
