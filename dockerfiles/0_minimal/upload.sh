175ed047647052719157c153f37b5c0710f68528
Devon R <sansgorialis@gmail.com>
2020-08-20 03:10:07 +0900

    Update Python versions

--- dockerfiles/0_minimal/upload.sh
+++ dockerfiles/0_minimal/upload.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 echo "Uploading stage 0 (minimal)"
 
 if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build0-$1-buster; then echo -e "UPLOAD build0-$1-buster" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build0-$1-buster" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
-if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build0-$1-slim-buster; then echo -e "UPLOAD build0-$1-slim-buster" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build0-$1-slim-buster" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
 if docker push ${DOCKER_UPLOAD_REPOSITORY:-gorialis}/discord.py:build0-$1-alpine; then echo -e "UPLOAD build0-$1-alpine" >> ${DOCKER_DISCORD_PY_PASSTMP:-/dev/null}; else echo -e "FAIL UPLOAD build0-$1-alpine" | tee -a ${DOCKER_DISCORD_PY_FAILTMP:-/dev/null}; fi
